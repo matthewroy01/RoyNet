8f345fd6981577445ad4f9ebd9ba744087b16506
MatthewRoy01 <matthew.roy01@mymail.champlain.edu>
2019-04-08 13:45:10 -0400

    added test project so networking can be run separately from dll

---
+++ TestingStandalone/TestingStandalone/main.cpp
@@ -0,0 +1,81 @@
+#include "../../Plugin/RoyNetPlugin/RoyNetPlugin/Server.h"
+#include "../../Plugin/RoyNetPlugin/RoyNetPlugin/Client.h"
+
+#include <iostream>
+
+Peer* netInstance = NULL;
+
+int rnStart(int isServer)
+{
+	// check if we're server or client
+	if (isServer == 1)
+	{
+		netInstance = new Server();
+		std::cout << "server start" << std::endl;
+	}
+	else
+	{
+		netInstance = new Client();
+		std::cout << "client start" << std::endl;
+	}
+
+	// initialize the instance
+	netInstance->Initialize();
+
+	return 0;
+}
+
+int rnUpdate()
+{
+	// receive messages
+	netInstance->NetworkingReceive();
+
+	return 0;
+}
+
+int rnStop()
+{
+	// shut down networking by destroying peer interface instance
+	if (netInstance != NULL)
+	{
+		RakNet::RakPeerInterface::DestroyInstance(netInstance->peer);
+		delete netInstance;
+		netInstance = NULL;
+		return 0;
+	}
+
+	return 1;
+}
+
+int main()
+{
+	bool correctInput = false;
+	char tmp;
+
+	while (!correctInput)
+	{
+		std::cout << "Enter 'C' for Client, 'S' for Server: ";
+		std::cin >> tmp;
+
+		if (tmp == 'C' || tmp == 'c')
+		{
+			rnStart(0);
+			correctInput = true;
+		}
+		else if (tmp == 'S' || tmp == 's')
+		{
+			rnStart(1);
+			correctInput = true;
+		}
+		else
+		{
+			std::cout << "Invalid input..." << std::endl;
+		}
+	}
+
+	std::cin.get();
+
+	rnStop();
+
+	std::cin.get();
+}
